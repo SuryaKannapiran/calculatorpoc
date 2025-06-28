6f97674cf1a1d9610903c6d14ee729812ca81f99
theindiedev3 <kannapiransurya@gmail.com>
2025-06-29 00:19:11 +0530

    railway deployment prep

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'calculatorpoc',
+  location: 'us-central1'
+};
+
--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
-import GenericCalculator from './packages/calculator/GenericCalculator';
 import LayoutEditor from './packages/layouteditor/LayoutEditor';
-import Navigation from './components/Navigation';
-import { pricingData } from './packages/calculator/data/pricingData';
 import { defaultLayout } from './packages/layouteditor/data/defaultLayout';
 
 function App() {
-  const [currentScreen, setCurrentScreen] = useState('calculator');
   const [currentLayout, setCurrentLayout] = useState(defaultLayout);
 
   const handleLayoutUpdate = (updatedLayout) => {
     setCurrentLayout(updatedLayout);
   };
 
-  const handleScreenChange = (screen) => {
-    setCurrentScreen(screen);
-  };
-
   return (
     <div className="app">
-      <Navigation 
-        currentScreen={currentScreen} 
-        onScreenChange={handleScreenChange} 
+      <LayoutEditor 
+        currentLayout={currentLayout}
+        onLayoutUpdate={handleLayoutUpdate}
       />
-      
-      {currentScreen === 'calculator' && (
-        <GenericCalculator pricingData={pricingData} />
-      )}
-      
-      {currentScreen === 'layout-editor' && (
-        <LayoutEditor 
-          currentLayout={currentLayout}
-          onLayoutUpdate={handleLayoutUpdate}
-        />
-      )}
     </div>
   );
 }
